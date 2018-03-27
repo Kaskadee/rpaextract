d563e18af84a8d6e072ff7ac90b654b1cc6c6938
Unknown <kaskadee0@gmail.com>
2018-03-27 11:16:50 +0200

    Code cleanup

--- rpaextract/Archive.cs
+++ rpaextract/Archive.cs
@@ -15,33 +15,35 @@ using rpaextract.Pickle;
 using SharpCompress.Compressors;
 using SharpCompress.Compressors.Deflate;
 
-namespace rpaextract
-{
+namespace rpaextract {
     /// <summary>
-    /// Provides a wrapper to easily access files in an RPA archive. (Ren'py Archives)
+    ///     Provides a wrapper to easily access files in an RPA archive. (Ren'py Archives)
     /// </summary>
-    internal sealed class Archive
-    {
+    internal sealed class Archive {
         private readonly FileInfo _info;
         private int _deobfuscationKey;
+
         /// <summary>
-        /// Gets the file name of the loaded archive.
+        ///     Gets the file name of the loaded archive.
         /// </summary>
         public string FileName { get; }
+
         /// <summary>
-        /// Gets the RPA archive version.
+        ///     Gets the RPA archive version.
         /// </summary>
         public int Version { get; }
+
         /// <summary>
-        /// Gets the list of file indices that are contained in the archive.
+        ///     Gets the list of file indices that are contained in the archive.
         /// </summary>
         public ArchiveIndex[] Indices { get; }
+
         /// <summary>
-        /// Initializes a new instance of the <see cref="Archive"/> class and loads archive information from the specified file.
+        ///     Initializes a new instance of the <see cref="Archive" /> class and loads archive information from the specified
+        ///     file.
         /// </summary>
         /// <param name="info">The file information of the RPA archive.</param>
-        public Archive(FileInfo info)
-        {
+        public Archive(FileInfo info) {
             _info = info;
             FileName = info.Name;
             // Determine archive version by reading the file header
@@ -52,59 +54,58 @@ namespace rpaextract
             // Extract file indices from archive
             Indices = ExtractIndices().ToArray();
         }
+
         /// <summary>
-        /// Returns true if the loaded archive is valid.
+        ///     Returns true if the loaded archive is valid.
         /// </summary>
-        public bool IsValid()
-        {
+        public bool IsValid() {
             return Version > 0 && Version <= 3;
         }
+
         /// <summary>
-        /// Gets the list of files contained in the archive.
+        ///     Gets the list of files contained in the archive.
         /// </summary>
         public string[] GetFileList()
         {
             return IsValid() ? Indices.Select(x => x.FilePath).ToArray() : null;
         }
+
         /// <summary>
-        /// Reads the specified file from the archive.
+        ///     Reads the specified file from the archive.
         /// </summary>
         /// <param name="index">The archive index of the file to read from.</param>
         /// <returns>The contents of the file as a byte-array</returns>
-        public byte[] Read(ArchiveIndex index)
-        {
+        public byte[] Read(ArchiveIndex index) {
             // Check if file exists in loaded indices
             if (index == null || !Indices.Contains(index))
                 throw new FileNotFoundException($"{index?.FilePath} couldn't be found in the archive!");
-            using (var archiveFs = _info.OpenRead())
-            {
-                using (var br = new BinaryReader(archiveFs, Encoding.UTF8))
-                {
+            using (var archiveFs = _info.OpenRead()) {
+                using (var br = new BinaryReader(archiveFs, Encoding.UTF8)) {
                     // Seek to calculated file offset
                     archiveFs.Seek(index.Offset, SeekOrigin.Begin);
                     // Read file contents from archive
                     var data = br.ReadBytes(index.Length - index.Prefix.Length);
-                    // Merge prefix and data to one data block
+                    // Merge prefix and data to one data block.
                     var completeData = new byte[index.Prefix.Length + data.Length];
                     Buffer.BlockCopy(index.Prefix, 0, completeData, 0, index.Prefix.Length);
                     Buffer.BlockCopy(data, 0, completeData, index.Prefix.Length, data.Length);
-                    // Cleanup unused data
+                    // Cleanup unused data.
                     Array.Clear(data, 0, data.Length);
                     Array.Clear(index.Prefix, 0, index.Prefix.Length);
                     return completeData;
                 }
             }
         }
+
         /// <summary>
-        /// Determines the archive version from its file header.
+        ///     Determines the archive version from its file header.
         /// </summary>
         /// <param name="info">The file information about the archive.</param>
         /// <returns>The archive version of the specified archive.</returns>
         /// <exception cref="FileNotFoundException">Thrown if the specified file doesn't exist.</exception>
         /// <exception cref="ArgumentOutOfRangeException">Thrown if the file is too small.</exception>
         /// <exception cref="InvalidDataException">Thrown if the specified file isn't a (supported) RPA archive.</exception>
-        private int GetArchiveVersion(FileInfo info)
-        {
+        private int GetArchiveVersion(FileInfo info) {
             // Check if the archive exists
             if (!info.Exists || info.Attributes.HasFlag(FileAttributes.Directory))
                 throw new FileNotFoundException("The specified archive couldn't be found!", info.FullName);
@@ -112,8 +113,7 @@ namespace rpaextract
             if (info.Length < 51)
                 throw new ArgumentOutOfRangeException(nameof(info), "The file is too small to be a valid archive!");
             // Open the specified file in read-only mode
-            using (var fs = info.OpenRead())
-            {
+            using (var fs = info.OpenRead()) {
                 // Read the first seven bytes from the file (this is the archive version)
                 var header = fs.ReadLine();
                 // Determine the archive version
@@ -127,20 +127,19 @@ namespace rpaextract
                 throw new InvalidDataException("The specified file isn't a (supported) RPA archive!");
             }
         }
+
         /// <summary>
-        /// Extracts file indicies from the archive.
+        ///     Extracts information about all file indicies from the archive.
         /// </summary>
         /// <returns>A list of all file indicies found in the archive.</returns>
-        private IEnumerable<ArchiveIndex> ExtractIndices()
-        {
+        private IEnumerable<ArchiveIndex> ExtractIndices() {
             var parser = new PickleParser();
             // Open archive in read-only mode
-            using (var fs = _info.OpenRead())
-            {
-                // Read header line and split it
+            using (var fs = _info.OpenRead()) {
+                // Read header line and split it at whitespace.
                 var header = fs.ReadLine();
-                var splitted = header.Split(' ');
-                // Retrieve hexadecimal offset and convert it to an integer value
+                var splitted = header.Split((char) 0x20);
+                // Retrieve hexadecimal offset and convert it to an integer value.
                 var offset = Convert.ToInt32(splitted[1], 16);
                 // Seek to the determined offset
                 fs.Seek(offset, SeekOrigin.Begin);
@@ -152,34 +151,33 @@ namespace rpaextract
                 var decompressed = stream.ReadToEnd();
                 // Unpickle the decompressed data
                 var deserialized = parser.Unpickle(decompressed);
-                if (Version == 3)
-                {
-                    // If this is an RPA-3.0 archive additionally calculate the deobfuscation key
-                    CalculateDeobfuscationKey(splitted);
-                    // Deobfuscate archive indicies
-                    deserialized = Deobfuscate(deserialized);
-                }
+                if (Version != 3) return deserialized;
+                // If this is an RPA-3.0 archive additionally calculate the deobfuscation key.
+                CalculateDeobfuscationKey(splitted);
+                // Deobfuscate archive indicies.
+                deserialized = Deobfuscate(deserialized);
                 return deserialized;
             }
         }
+
         /// <summary>
-        /// Deobfuscates the specified archive indices.
+        ///     Deobfuscates the specified archive indices.
         /// </summary>
-        /// <param name="indices">The obfuscated archive indices.</param>
-        private IEnumerable<ArchiveIndex> Deobfuscate(IEnumerable<ArchiveIndex> indices)
-        {
-            // Apply deobfuscation key by using binary XOR on offset and length
-            return indices.Select(ind => new ArchiveIndex(ind.FilePath, ind.Offset ^ _deobfuscationKey, ind.Length ^ _deobfuscationKey, ind.Prefix));
+        /// <param name="indices">The list of obfuscated archive indices.</param>
+        private IEnumerable<ArchiveIndex> Deobfuscate(IEnumerable<ArchiveIndex> indices) {
+            // Apply deobfuscation key by using binary XOR on offset and length.
+            return indices.Select(ind => new ArchiveIndex(ind.FilePath, ind.Offset ^ _deobfuscationKey,
+                ind.Length ^ _deobfuscationKey, ind.Prefix));
         }
+
         /// <summary>
-        /// Calculates the deobfuscation key for this archive.
+        ///     Calculates the deobfuscation key for this archive.
         /// </summary>
-        /// <param name="splittedHeader">The splitted header array.</param>
-        private void CalculateDeobfuscationKey(IEnumerable<string> splittedHeader)
-        {
-            // Reset key to zero
+        /// <param name="splittedHeader">The archive header as an splitted array.</param>
+        private void CalculateDeobfuscationKey(IEnumerable<string> splittedHeader) {
+            // Reset key to zero.
             _deobfuscationKey = 0;
-            // Calculate key from archive header
+            // Calculate key from archive header.
             foreach (var subkey in splittedHeader.Skip(2))
                 _deobfuscationKey ^= Convert.ToInt32(subkey, 16);
         }
--- rpaextract/Extensions/StreamExtensions.cs
+++ rpaextract/Extensions/StreamExtensions.cs
@@ -1,41 +1,37 @@
 ﻿using System.IO;
 using System.Text;
 
-namespace rpaextract.Extensions
-{
+namespace rpaextract.Extensions {
     /// <summary>
-    /// Provides methods for extending the <see cref="Stream"/> class and its derivatives.
+    ///     Provides methods for extending the <see cref="Stream" /> class and its derivatives.
     /// </summary>
-    public static class StreamExtensions
-    {
+    public static class StreamExtensions {
         /// <summary>
-        /// Reads a string, from the specified stream, that is terminated by a line-break.
+        ///     Reads a string, from the specified stream, that is terminated by a line-break.
         /// </summary>
         /// <param name="stream">The stream to read the string from.</param>
         /// <returns>The read string without the line-break at the end of the string.</returns>
         /// <remarks>The position of the stream will be advanced to the point after the line-break character.</remarks>
-        public static string ReadLine(this Stream stream)
-        {
+        public static string ReadLine(this Stream stream) {
             var sb = new StringBuilder();
             // Search until line-break is found or end of stream has been reached
-            while (stream.Position < stream.Length)
-            {
-                var newChar = (char)stream.ReadByte();
+            while (stream.Position < stream.Length) {
+                var newChar = (char) stream.ReadByte();
                 if (newChar == '\n')
                     return sb.ToString();
                 sb.Append(newChar);
             }
+
             // Reached end of stream and no line-break has been found.
             return sb.ToString();
         }
+
         /// <summary>
-        /// Reads all bytes until the end of stream, from the current position of the stream, has been reached.
+        ///     Reads all bytes until the end of stream, from the current position of the stream, has been reached.
         /// </summary>
         /// <param name="stream">The stream to read from.</param>
-        public static byte[] ReadToEnd(this Stream stream)
-        {
-            using (var ms = new MemoryStream())
-            {
+        public static byte[] ReadToEnd(this Stream stream) {
+            using (var ms = new MemoryStream()) {
                 // Default buffer size is set to 4096 bytes.
                 var buffer = new byte[4096];
                 int count;
