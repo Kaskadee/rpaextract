ed1f70e55488b82edea43052ef08d163c403e718
Fabian Creutz <kaskadee0@gmail.com>
2020-05-20 00:40:05 +0200

    Added ArchiveVersion for better version handling

---
+++ csharp/rpaextract/ArchiveVersion.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace rpaextract {
+    /// <summary>
+    ///     Provides an enumeration of all possible Ren'py archive version that are known in this version.
+    /// </summary>
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public enum ArchiveVersion {
+        /// <summary>
+        ///     Unknown version.
+        /// </summary>
+        Unknown,
+        /// <summary>
+        ///     Initial ren'py archive version. Not supported.
+        /// </summary>
+        RPI = 1,
+        /// <summary>
+        ///     RPA-2.0 archive.
+        /// </summary>
+        RPA2 = 2,
+        /// <summary>
+        ///     RPA-3.0 archive.
+        /// </summary>
+        RPA3 = 3,
+        /// <summary>
+        ///     RPA-3.2 archive.
+        /// </summary>
+        RPA32 = 4
+    }
+}
